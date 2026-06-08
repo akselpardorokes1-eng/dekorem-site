7a203f40a44664902c04babd714c236dafeba20d
Aksel Pardo Rokes <akselpardorokes2@gmail.com>
2026-06-08 11:51:48 +0300

    homepage redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/prototype.html',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
